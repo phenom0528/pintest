8f27170eef453ccaa70c853bd7a30c3db8143934
phenom0528 <64686250+phenom0528@users.noreply.github.com>
2020-05-02 20:50:47 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,26 @@
 /**
- * Functions are mapped to blocks using various macros 
- * in comments starting with % (e.g., //% block).
- * The most important macro "block" specifies that a
- * block should be generated for a **exported** function.
+ * Functions are mapped to blocks using various macros in comments starting with % (e.g., //% block).
+ * The most important macro "block" specifies that a block should be generated for a **exported** function.
  */
-//% block="pintest" color="#55278D"
-namespace pintest {
+ 
+//% block="pintest color="#44278D" 
 
-    //weight=20
-    //% blockId=digital_wirtepin
-    //% block="digital write pin |%tname|to|%tvalue|"
-    //%tname.fieldEditor="gridpicker"
-    //%tname.fieldOptuions.columns=3
-    //%tvalue.defl=0
-    //%tvalue.min=0
-    //%tvalue.max=0
-export function digital_wirtepin(tname:Digitalpin, tvalue: number ): void{ 
-    pins.digitalWritePin(tname,tavlue)
+namespace jcjh1 {
+
+
+    //% weight=20
+    //% blockId=digital_writepin 
+    //% block="digital write pin |%tname| to |%tvalue"
+    //% tname.fieldEditor="gridpicker" 
+    //% tname.fieldOptions.columns=3 
+    //% tvalue.defl=0
+    //% tvalue.min=0
+    //% tvalue.max=1	
+    export function digital_writepin(tname: DigitalPin, tvalue: number): void {
+        pins.digitalWritePin(tname, tvalue)
+    }
+
+
+
 }
+© 2020 GitHub, Inc.
